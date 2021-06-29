4434496624bc6a03f0546611fc6b5fe54c9a4f3f
FarizAditya <farizokzz21@gmail.com>
2021-06-29 21:16:12 +0700

    new file

--- js/main.js
+++ js/main.js
@@ -81,7 +81,7 @@ $(document).ready(function () {
 //register serviceworker
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
-    navigator.serviceWorker.register("/service-worker.js").then(
+    navigator.serviceWorker.register("./service-worker.js", { scope: "./" }).then(
       function (registration) {
         // Registration was successful
         console.log("ServiceWorker registration successful with scope: ", registration.scope);
--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,50 @@
 var CACHE_NAME = "my-site-cache-v1";
 var urlsToCache = ["/", "./index.html", "./api.html", "./fallback.json", "./css/style.css", "./js/jquery.min.js", "./js/main.js", "./images/logo.png"];
 
+var CACHE_VERSION = 1;
+var CURRENT_CACHES = {
+  prefetch: "prefetch-cache-v" + CACHE_VERSION,
+};
+
 self.addEventListener("install", function (event) {
+  var urlsToPrefetch = ["./index.html", "./api.html", "./css/style.css", "./images/logo.png"];
+
+  console.log("Handling install event. Resources to pre-fetch:", urlsToPrefetch);
+
   event.waitUntil(
-    caches.open(staticCacheName).then((staticCache) => {
-      // fichiers souhaités en cache
-      staticCache.addAll(["./index.html", "./api.html", "./fallback.json", "./css/style.css", "./js/main.js", "./js/jquery.min.js"]);
-      // fichiers à mettre impérativement en cache
-      return staticCache.addAll(["./index.html", "./api.html", "./fallback.json", "./css/style.css", "./js/main.js", "./    js/jquery.min.js"]);
-    })
+    caches
+      .open(CURRENT_CACHES["prefetch"])
+      .then(function (cache) {
+        return cache
+          .addAll(
+            urlsToPrefetch.map(function (urlToPrefetch) {
+              return new Request(urlToPrefetch, { mode: "no-cors" });
+            })
+          )
+          .then(function () {
+            console.log("All resources have been fetched and cached.");
+          });
+      })
+      .catch(function (error) {
+        console.error("Pre-fetching failed:", error);
+      })
   );
 });
 
 // Menyimpan cache dan mengembalikan permintaan
 self.addEventListener("fetch", function (event) {
-  var request = event.request;
-  var url = new URL(request.url);
-
-  // Memisahkan request API dan Internal
-  if (url.origin === location.origin) {
-    event.respondWith(
-      caches.match(request).then(function (response) {
-        return response || fetch(request);
-      })
-    );
-  } else {
-    event.respondWith(
-      caches.open("project-cache").then(function (cache) {
-        return fetch(request)
-          .then(function (LiveResponse) {
-            cache.put(request, LiveResponse.clone());
-            return LiveResponse;
-          })
-          .catch(function () {
-            return caches.match(request).then(function (response) {
-              if (response) return response;
-              return caches.match("/fallback.json");
-            });
-          });
-      })
-    );
-  }
+  event.respondWith(
+    (async function () {
+      try {
+        var res = await fetch(event.request);
+        var cache = await caches.open("cache");
+        cache.put(event.request.url, res.clone());
+        return res;
+      } catch (error) {
+        return caches.match(event.request);
+      }
+    })()
+  );
 });
 // update serviceworker
 self.addEventListener("activate", function (event) {
